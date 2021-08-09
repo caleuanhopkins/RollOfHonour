a05b7fcbabd8cb9c1e14200ca1d8f674850ed4e0
Morgan McDonald <morganmcdee13@outlook.com>
2021-08-09 19:16:02 +0100

    Update 09/08/21

--- bain.js
+++ bain.js
@@ -1,20 +1,47 @@
 const data = {
   labels: [
-      'Aaron (7)',
-      'Jaay (6)',
-      'Dude (2)',
-      'Rotary (1)',
+      'Jaay (37)',
+      'Aaron (27)',
+      'Sulk (10)',
+      'Rotary (10)',
+      'Xainarc (8)',
+      'Cal (8)',
+      'Heel (7)',
+      'Aidan (6)',
+      'Morgan (4)',
+      'Rosco (4)',
+      'Mags (3)',
+      'Sunny (3)',
+      'Goldbuggg (3)',
+      'Aif (2)',
+      'Dazza_G (2)',
+      'Gurne (2)',
+      'Pudge (1)',
+      'HYB (1)',
       'Stu (1)',
-      'Xainarc (1)',
-      'Morgan (1)',
-      'Aif (1)',
-      'Gurne (1)',
-      'Heel (1)'
+      'Lainncli (1)',
+      'Sweaty (1)'
     ],
 datasets: [
   {
     label: "Total Freddos",
-    data: [52, 38, 13, 8, 7, 7, 7, 5, 4, 4],
+    data: [225, 161, 55, 45, 51, 40, 27, 36, 24, 19, 21, 18, 15, 15, 12, 8, 10, 8, 7, 6, 4],
+    backgroundColor: [
+      "rgba(102, 209, 110, 0.2)",
+      "rgba(201, 203, 207, 0.2)",
+      "rgba(255, 205, 86, 0.2)",
+
+    ],
+    borderColor: [
+      "rgb(102, 209, 110)",
+      "rgb(201, 203, 207)",
+      "rgb(255, 205, 86)",
+    ],
+    borderWidth: 1,
+  },
+  {
+    label: "Average Freddo Per Message",
+    data: [6.1, 6, 5.5, 4.5, 6.4, 5, 3.9, 6, 6, 4.8, 7, 6, 5, 7.5, 6, 4, 10, 8, 7, 6, 4],
     backgroundColor: [
       "rgba(102, 209, 110, 0.2)",
       "rgba(201, 203, 207, 0.2)",
